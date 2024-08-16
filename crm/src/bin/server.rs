9227b78e8fd492e066050c4dc191c3fc39ac761d
xyzBits <lidf077@gmail.com>
2024-08-16 20:50:58 +0800

    initial user-stat

--- crm/src/bin/server.rs
+++ crm/src/bin/server.rs
@@ -8,6 +8,8 @@ use tonic::{async_trait, Request, Response, Status};
 #[derive(Default)]
 pub struct UserServer;
 
+// 这个 UserService 是定义在 protobuf 中的 UserService
+// 以前 async 不能在trait 中使用，所以需要加这个注解，才可以在实现中添加 async 函数
 #[async_trait]
 impl UserService for UserServer {
     async fn get_user(&self, request: Request<GetUserRequest>) -> Result<Response<User>, Status> {
