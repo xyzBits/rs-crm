3213b26319b53861dbd9983a06f8f4ae74ad3c51
xyzBits <lidf077@gmail.com>
2024-08-17 22:24:07 +0800

    feature: support generate dummy data for user stats

--- user-stat/examples/gen.rs
+++ user-stat/examples/gen.rs
@@ -7,7 +7,10 @@ use fake::{Dummy, Fake, Faker};
 use nanoid::nanoid;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
+use sqlx::{Executor, PgPool};
+use std::collections::HashSet;
 use std::hash::{Hash, Hasher};
+use tokio::time::Instant;
 // generate 10_000 users and run the in a tx, repeat 500 times
 
 #[derive(Debug, Clone, Dummy, Serialize, Deserialize, PartialEq, Eq)]
@@ -16,7 +19,7 @@ enum Gender {
     Male,
     Unknown,
 }
-#[derive(Debug, Clone, Dummy, Serialize, Deserialize)]
+#[derive(Debug, Clone, Dummy, Serialize, Deserialize, Eq, PartialEq)]
 struct UserStat {
     #[dummy(faker = "UniqueEmail")]
     email: String,
@@ -59,22 +62,83 @@ struct UserStat {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let user: UserStat = Faker.fake();
+    let pool = PgPool::connect("postgres://postgres:pass123@localhost:5432/stats").await?;
 
-    println!("{:?}", user);
+    for i in 1..=500 {
+        let users: HashSet<_> = (0..10000).map(|_| Faker.fake::<UserStat>()).collect();
+
+        let start = Instant::now();
+        raw_insert(users, &pool).await?;
+        // bulk_insert(users, &pool).await?;
+        println!("Batch {} inserted in {:?}", i, start.elapsed());
+    }
+
     Ok(())
 }
 
-async fn raw_insert(user: UserStat) -> Result<()> {
+async fn raw_insert(users: HashSet<UserStat>, pool: &PgPool) -> Result<()> {
+    let mut sql = String::with_capacity(10 * 1000 * 1000);
+    sql.push_str("
+    INSERT INTO user_stats(email, name, created_at, last_visited_at, last_watched_at, recent_watched, viewed_but_not_started, started_but_not_finished, finished, last_email_notification, last_in_app_notification, last_sms_notification)
+    VALUES");
+
+    for user in users {
+        sql.push_str(&format!(
+            "('{}', '{}', '{}', '{}', '{}', {}::int[], {}::int[], {}::int[], {}::int[], '{}', '{}', '{}'),",
+            user.email,
+            user.name,
+            user.created_at,
+            user.last_visited_at,
+            user.last_watched_at,
+            list_to_string(user.recent_watched),
+            list_to_string(user.viewed_but_not_started),
+            list_to_string(user.started_but_not_finished),
+            list_to_string(user.finished),
+            user.last_email_notification,
+            user.last_in_app_notification,
+            user.last_sms_notification,
+        ));
+    }
+
+    let v = &sql[..sql.len() - 1];
+    sqlx::query(v).execute(pool).await?;
+
     Ok(())
 }
 
 fn list_to_string(list: Vec<i32>) -> String {
-    todo!()
+    format!("ARRAY{:?}", list)
 }
 
 #[allow(dead_code)]
-async fn bulk_insert(users: Vec<UserStat>) -> Result<()> {
+async fn bulk_insert(users: HashSet<UserStat>, pool: &PgPool) -> Result<()> {
+    let mut transaction = pool.begin().await?;
+
+    for user in users {
+        let query = sqlx::query(
+            r#"
+            INSERT INTO user_stats(email, name, created_at, last_visited_at, last_watched_at, recent_watched, viewed_but_not_started, started_but_not_finished, finished, last_email_notification, last_in_app_notification, last_sms_notification)
+            VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+           "#
+        )
+            .bind(&user.email)
+            .bind(&user.name)
+            .bind(user.created_at)
+            .bind(user.last_visited_at)
+            .bind(user.last_watched_at)
+            .bind(&user.recent_watched)
+            .bind(&user.viewed_but_not_started)
+            .bind(&user.started_but_not_finished)
+            .bind(&user.finished)
+            .bind(user.last_email_notification)
+            .bind(user.last_in_app_notification)
+            .bind(user.last_sms_notification)
+            ;
+
+        transaction.execute(query).await?;
+    }
+
+    transaction.commit().await?;
     Ok(())
 }
 
