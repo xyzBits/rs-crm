598b0697c83ba0edd7c8b75915a42f4f1cd1c567
xyzBits <lidf077@gmail.com>
2024-08-22 09:30:22 +0800

    impl metadata

--- crm-metadata/src/lib.rs
+++ crm-metadata/src/lib.rs
@@ -4,6 +4,40 @@ pub mod pb;
 pub mod config;
 
 pub use abi::*;
+use futures::Stream;
 pub use pb::*;
+use std::pin::Pin;
+use tonic::{Request, Response, Status, Streaming};
 
+use crate::metadata_server::{Metadata, MetadataServer};
 pub use config::*;
+
+#[allow(unused)]
+pub struct MetadataService {
+    config: AppConfig,
+}
+
+type ServiceResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<Content, Status>> + Send>>;
+
+impl Metadata for MetadataService {
+    type MaterializeStream = ResponseStream;
+
+    async fn materialize(
+        &self,
+        request: Request<Streaming<MaterializeRequest>>,
+    ) -> ServiceResult<Self::MaterializeStream> {
+        let request = request.into_inner();
+        self.materialize(request).await
+    }
+}
+
+impl MetadataService {
+    pub fn new(config: AppConfig) -> Self {
+        MetadataService { config }
+    }
+
+    pub fn into_server(self) -> MetadataServer<Self> {
+        MetadataServer::new(self)
+    }
+}
